57f59201c2f432b94dc8f189f6905c0f7a7b4c6a
“etekoe222” <edemtekoe@gmail.com>
2024-07-29 15:56:52 -0500

    w10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "richau22"
+    bucket         = "rrtryuujjj"
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    #dynamodb_table = "josh-2017"
+    dynamodb_table = "jnhffdgh"
   }
 }
